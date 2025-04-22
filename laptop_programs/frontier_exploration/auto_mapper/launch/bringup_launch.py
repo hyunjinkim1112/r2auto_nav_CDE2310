27b573671784c389079bd6a9f0a94777b03c7e15
Mayukh Ghosh <mayukhghosh1975@gmail.com>
2025-04-22 12:59:13 +0800

    Create v1.0.0 bringup_launch.py

--- laptop_programs/frontier_exploration/auto_mapper/launch/bringup_launch.py
+++ laptop_programs/frontier_exploration/auto_mapper/launch/bringup_launch.py
@@ -13,7 +13,6 @@
 # limitations under the License.
 
 import os
-
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import (DeclareLaunchArgument, GroupAction,
